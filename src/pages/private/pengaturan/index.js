32e1887a45fae51cdd734a17fdfcaa6801321e9a
angfau <angga.fauzi23@gmail.com>
2020-03-30 02:08:35 +0700

    Route dom

--- src/pages/private/pengaturan/index.js
+++ src/pages/private/pengaturan/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch, Route, Redirect} from 'react-router-dom';
 import Pengguna from './pengguna';
-import Toko from './Toko';
+import Toko from './toko';
 
 function Pengaturan(){
     return(
         <Switch>
             <Route path="/pengaturan/pengguna" component={Pengguna} />
             <Route path="/pengaturan/toko" component={Toko} />
-
+            <Redirect to="pengaturan/pengguna"/>
         </Switch>
     )
 }
